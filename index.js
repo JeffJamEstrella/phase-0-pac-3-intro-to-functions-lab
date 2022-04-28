79ad891a3e3af7a08aacd56f60cc191ba6c5e069
Jeffrey Estrella <jeffrey.jrp.estrella@gmail.com>
2022-04-28 23:42:05 +1000

    Finally did the functions!

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout (hello) {
+    console.log (hello.toUpperCase());
+}
+
+function logWhisper (hello) {
+    console.log (hello.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate (string) {
+    if (string === "hello")
+        return "I can\'t hear you!"
+            else if (string === "HELLO")
+        return "YES INDEED!"
+            else if (string === "Let's have dinner together!")
+            return "I would love to!"
+}
